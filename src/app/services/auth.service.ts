1a3ab829899388bbfc6e29932a31534dcf83eb2c
n-rick <e.ficadiere@gmail.com>
2022-06-23 23:42:32 +0200

    chore: add service auth and mlovie service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../interfaces/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private url: string = "https://127.0.0.1:8000/authentication_token";
+
+  constructor(private htpp: HttpClient) {
+  }
+
+  login(user: User) {
+    return this.htpp.post<{ token: string }>(this.url, user);
+  }
+}
--- src/app/services/film.service.ts
+++ src/app/services/film.service.ts
@@ -6,12 +6,16 @@ import { Film } from '../interfaces/film';
   providedIn: 'root'
 })
 export class FilmService {
-  
-  private url:string = "https://127.0.0.1:8000/api/film"
 
-  constructor(private http: HttpClient) { }
+  private url: string = "https://127.0.0.1:8000/api/films";
+  private headers: HttpHeaders;
 
-  getAllFilms(){
-    return this.http.get<Array<Film>>(this.url);
+  constructor(private http: HttpClient) {
+    const token = localStorage.getItem('tokm2iflix');
+    this.headers = new HttpHeaders().set("Authorization", 'Bearer' + token);
+  }
+
+  getAllFilms() {
+    return this.http.get<Array<Film>>(this.url, {headers: this.headers});
   }
 }
