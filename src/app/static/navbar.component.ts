9ff9f4e9d70023774d0dad6961300e2f2602a5a2
n-rick <e.ficadiere@gmail.com>
2022-06-24 11:47:52 +0200

    chore: update navbar

--- src/app/static/navbar.component.ts
+++ src/app/static/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../interfaces/user';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  tokm2iflix = localStorage.getItem('tokm2iflix');
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+
+  }
+  deconnexion() {
+    localStorage.removeItem('tokm2iflix');
+    this.router.navigateByUrl('connexion');
   }
 
 }
